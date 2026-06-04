3463e7d34b4130058d8c4f33b41072b68bd38158
zama200pi <arshijoh1368754@gmail.com>
2026-06-04 12:31:12 +0530

    exmple2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,39 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtCountdown;
+    Button btnPos;
+    Button btnNeg;
+
+    int counts=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        txtCountdown = findViewById(R.id.txtCountdown);
+        btnPos = findViewById(R.id.btnPos);
+        btnNeg = findViewById(R.id.btnNeg);
+
+        btnPos.setOnClickListener(v -> {
+
+            counts++;
+            txtCountdown.setText("count: "+counts);
+        });
+
+        btnNeg.setOnClickListener(v -> {
+
+            counts--;
+            txtCountdown.setText("count: "+counts);
         });
     }
 }
