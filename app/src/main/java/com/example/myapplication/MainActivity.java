52fa362db7dff53ceabe69e9da19cb81d4eeea51
zama200pi <arshijoh1368754@gmail.com>
2026-06-05 16:16:26 +0530

    lrning

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.myapplication;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText tsx;
     TextView txtCountdown;
     Button btnPos;
     Button btnNeg;
@@ -23,16 +25,20 @@ public class MainActivity extends AppCompatActivity {
         txtCountdown = findViewById(R.id.txtCountdown);
         btnPos = findViewById(R.id.btnPos);
         btnNeg = findViewById(R.id.btnNeg);
+        tsx=findViewById(R.id.edit_query);
 
         btnPos.setOnClickListener(v -> {
+            String s=tsx.getText().toString();
 
-            counts+=3;
+            if(!s.isEmpty()){counts+=Integer.parseInt(s);}
             txtCountdown.setText("count: "+counts);
         });
 
         btnNeg.setOnClickListener(v -> {
 
-            counts-=5;
+            String s=tsx.getText().toString();
+
+            if(!s.isEmpty()){counts-=Integer.parseInt(s);}
             txtCountdown.setText("count: "+counts);
         });
     }
